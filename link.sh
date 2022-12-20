6db495115ad859506c9ca6a6ef5cfd81cccc2cf1
Taisuke Araki <araky@TaisukenoMacBook-Air.local>
2022-12-20 13:32:18 +0900

    first commit

---
+++ link.sh
@@ -0,0 +1,9 @@
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+for dotfile in "${DOTFILES_DIR}"/.??* ; do
+    [[ "$dotfile" == "${DOTFILES_DIR}/.git" ]] && continue
+    [[ "$dotfile" == "${DOTFILES_DIR}/.github" ]] && continue
+    [[ "$dotfile" == "${DOTFILES_DIR}/.DS_Store" ]] && continue
+
+    ln -fnsv "$dotfile" "$HOME"
+done
